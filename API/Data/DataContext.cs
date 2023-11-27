b7d578cdfe5d0ae1ac53118b21c353a6938fb140
KTaraszovics <taraszovics.karoly@allwin.hu>
2023-11-27 09:25:00 +0100

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+public class DataContext : DbContext
+{
+public DbSet<AppUser> Users { get; set; }
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+}
